80bf5cf0fd69c31ef182d108ec722bb53c6f362b
Vincent Liu <binhua1@ualberta.ca>
2024-03-05 17:37:46 -0700

    add tests

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -12,5 +12,41 @@ import java.util.ArrayList;
 
 public class CustomListTest {
 
-
+    /**
+     * this gets size of the list
+     * @return number of cities
+     */
+    public int getCount(){
+        return cities.size();
+    }
+    /**
+     * this adds a city object to the list
+     *for the first phase it will be
+     empty * @param city
+     */
+    public void addCity(City city){
+        cities.add(city);
+    }
+    private CustomList list;
+    /**
+     * create a mocklist for my citylist
+     * @return list
+     */
+    public CustomList MockCityList(){
+        list = new CustomList(null,new ArrayList<>());
+        return list;
+    }
+    /**
+     * get the size of the list
+     * increase the list by adding a new city
+     * check if our current size matches the initial size
+     plus one
+     */
+    @Test
+    public void addCityTest(){
+        list = MockCityList();
+        int listSize = list.getCount();
+        list.addCity(new City("Estevan", "SK"));
+        assertEquals(list.getCount(),listSize + 1);
+    }
 }
